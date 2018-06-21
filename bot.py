1764de925de0f4c48ba546dedfc24e8fae31b35d
PlopR <leeasher090@gmail.com>
2018-06-21 23:52:35 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,20 @@
 import discord
 import asyncio
 TOKEN = 'NDU4MTYxMjMzNjcwMDQ1NzEw.DgjqXg.ZYrnJyx7Fr_7-oTtNo8iOsrHD4o'
-continue = True
 client = discord.Client()
+keepGoing = True
 
 @client.event
 async def on_message(message):
     if message.content.startswith('james') or message.content.startswith('itai') or message.content.startswith('jude'):
         await client.send_message(message.channel, "This Bot will live forever")
     if message.content.startswith('loop'):
-        continue = True
-        while continue:
-            await client.send_message(message.channel, message.contents)
+        keepGoing = True
+        while keepGoing == True:
+            await client.send_message(message.channel, message.content)
             
     if message.content.startswith('stop'):
-        continue = False
+        keepGoing = False
   
     if  "pls" in message.content:
         await client.delete_message(message)
